b4c6000da3d8613414f34642887e8358d8f809d7
joesuffredini <gsuffred@gmail.com>
2021-04-21 14:46:14 -0400

    Adds changes

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,29 +3,21 @@
     <div id="student-data">
       <!-- v-for/v-bind image link {{student.photo}}-->
       <img src="" alt="" />
-      <!-- <h1>{{student.first_name}}</h1> -->
-      <h3>First Name</h3>
-      <!-- <h3>{{student.last_name}}</h3> -->
-      <h3>Last Name</h3>
-      <!-- <p>{{student.email}}</p> -->
-      <p>Email</p>
-      <!-- <p>{{student.phone}}</p> -->
+      <h3>{{ student.first_name }} {{ student.last_name }}</h3>
+      <!-- <h3>First Name</h3> -->
+      <h3></h3>
+      <!-- <h3>Last Name</h3> -->
+      <p>{{ student.email }}</p>
+      <!-- <p>Email</p> -->
+      <p>Phone Number: {{ student.phone }}</p>
       <p>Phone Number:</p>
-      <!-- <p>{{student.photo}}</p> -->
-      <p>Photo:</p>
-      <!-- <p>{{student.bio}}</p> -->
-      <p>Bio:</p>
-      <!-- <p>{{student.linkedin_url}}</p> -->
-      <p>Linkedin URL:</p>
-      <!-- <p>{{student.twitter_handle}}</p> -->
-      <p>Twitter:</p>
-      <!-- <p>{{student.website_url}}</p> -->
-      <p>Website:</p>
-      <!-- <p>{{student.resume_url}}</p> -->
-      <p>Resume:</p>
-      <!-- <p>{{student.github_url}}</p> -->
-      <p>Github:</p>
-      <!-- <p>{{student.bio}}</p> -->
+      <p>Profile Photo: {{ student.photo }}</p>
+      <p>Bio: {{ student.bio }}</p>
+      <p>Linked-In: {{ student.linkedin_url }}</p>
+      <p>Twitter: {{ student.twitter_handle }}</p>
+      <p>Website: {{ student.website_url }}</p>
+      <p>Resume: {{ student.resume_url }}</p>
+      <p>Github {{ student.github_url }}</p>
       <button type="button" v-on:click="editStudentModal">Edit</button>
       <dialog id="student-details">
         <form method="dialog">
@@ -81,29 +73,90 @@
     </div>
 
     <div id="student-experiences">
+      <h3>Experience:</h3>
+      <div id="experiences-list" v-for="experience in experiences" :key="experience.id">
+        <h4>{{ experience.company_name }}</h4>
+        <button type="button" v-on:click="editExperienceModal">Edit</button>
+        <dialog id="experience-details">
+          <form method="dialog">
+            <h1>Edit Your Experience</h1>
+            <p>
+              Start Date:
+              <input type="text" v-model="experiences.start_date" />
+            </p>
+            <p>
+              End Date:
+              <input type="text" v-model="experiences.end_date" />
+            </p>
+            <p>
+              Job Title:
+              <input type="text" v-model="experiences.job_title" />
+            </p>
+            <p>
+              Company Name:
+              <input type="text" v-model="experiences.company_name" />
+            </p>
+            <p>
+              Details:
+              <input type="text" v-model="experiences.details" />
+            </p>
+            <button v-on:click="updateExperience(experience)">Update</button>
+            <button v-on:click="destroyExperience(experience)">Delete</button>
+            <button>Close</button>
+          </form>
+        </dialog>
+        <!-- <button v-on:click="createWindow">Add Experience</button>
+        <dialog id="create-experience-window">
+          <form method="dialog">
+            <h1>New Experience</h1>
+            <div class="form-group">
+              <label>Start Date:</label>
+              <input type="text" class="form-control" v-model="experiences.startDate" />
+            </div>
+            <div class="form-group">
+              <label>End Date:</label>
+              <input type="text" class="form-control" v-model="experiences.endDate" />
+            </div>
+            <div class="form-group">
+              <label>Job Title:</label>
+              <input type="text" class="form-control" v-model="experiences.jobTitle" />
+            </div>
+            <div class="form-group">
+              <label>Company Name:</label>
+              <input type="text" class="form-control" v-model="experiences.company_name" />
+            </div>
+            <div class="form-group">
+              <label>Details:</label>
+              <input type="text" class="form-control" v-model="experiences.details" />
+            </div>
+            <button v-on:click="createExperience()">Submit</button>
+            <button>close</button>
+          </form>
+        </dialog> -->
+      </div>
       <button v-on:click="createWindow">Add Experience</button>
       <dialog id="create-experience-window">
         <form method="dialog">
           <h1>New Experience</h1>
           <div class="form-group">
             <label>Start Date:</label>
-            <input type="text" class="form-control" v-model="startDate" />
+            <input type="text" class="form-control" v-model="experiences.start_date" />
           </div>
           <div class="form-group">
             <label>End Date:</label>
-            <input type="text" class="form-control" v-model="endDate" />
+            <input type="text" class="form-control" v-model="experiences.end_date" />
           </div>
           <div class="form-group">
             <label>Job Title:</label>
-            <input type="text" class="form-control" v-model="jobTitle" />
+            <input type="text" class="form-control" v-model="experiences.job_title" />
           </div>
           <div class="form-group">
             <label>Company Name:</label>
-            <input type="text" class="form-control" v-model="companyName" />
+            <input type="text" class="form-control" v-model="experiences.company_name" />
           </div>
           <div class="form-group">
             <label>Details:</label>
-            <input type="text" class="form-control" v-model="details" />
+            <input type="text" class="form-control" v-model="experiences.details" />
           </div>
           <button v-on:click="createExperience()">Submit</button>
           <button>close</button>
@@ -112,17 +165,18 @@
     </div>
 
     <div class="student-education">
+      <h3>Education:</h3>
       <button v-on:click="showEducation()">Add Education</button>
       <dialog id="education-details">
         <form method="dialog">
           <h1>Education Experience</h1>
           <div class="form-group">
             <label>Start Date:</label>
-            <input type="text" class="form-control" v-model="start" />
+            <input type="text" class="form-control" v-model="start_date" />
           </div>
           <div class="form-group">
             <label>End Date:</label>
-            <input type="text" class="form-control" v-model="end" />
+            <input type="text" class="form-control" v-model="end_date" />
           </div>
           <div class="form-group">
             <label>Degree:</label>
@@ -141,18 +195,46 @@
         </form>
       </dialog>
     </div>
+
+    <div id="student-capstone">
+      <h3>Capstones:</h3>
+      <button v-on:click="createCapstoneWindow">Add Capstone</button>
+      <dialog id="create-capstone-window">
+        <h3>Capstone:</h3>
+        <form method="dialog">
+          <div class="form-group">
+            <label>Name:</label>
+            <input type="text" class="form-control" v-model="name" />
+          </div>
+          <div class="form-group">
+            <label>Description:</label>
+            <input type="text" class="form-control" v-model="description" />
+          </div>
+          <div class="form-group">
+            <label>URL:</label>
+            <input type="text" class="form-control" v-model="url" />
+          </div>
+          <div class="form-group">
+            <label>Screenshot:</label>
+            <input type="text" class="form-control" v-model="screenshot" />
+          </div>
+          <button v-on:click="createCapstone()">Submit</button>
+          <button v-on:click="updateCapstone()">Update</button>
+          <button>close</button>
+        </form>
+      </dialog>
+    </div>
+
     <div id="student-skills">
       <h3>Skills:</h3>
-    </div>
-    <div id="student-capstone">
-      <form v-on:submit.prevent="createSkill()">
+      <form v-on:submit.prevent="createSkills()">
         <input v-model="new_skill" type="text" id="skill-input" placeholder="CRUD Apps, Ruby, etc.¸" />
         <button type="submit">Add Skill</button>
       </form>
-      <div id="skills-list" v-for="skill in skills_list" :key="skill">
+      <div id="skills-list" v-for="skill in orderdSkills" :key="skill.id">
         <ul>
           <li>
-            {{ skill }}
+            {{ skill.name }}
             <button v-on:click="destroySkill(skill)">Delete</button>
           </li>
         </ul>
@@ -165,15 +247,17 @@
 
 <script>
 import axios from "axios";
+// import func from "vue-editor-bridge";
 export default {
   data: function () {
     return {
       student: {},
+      skills: {},
       experiences: {
-        startDate: "",
-        endDate: "",
-        jobTitle: "",
-        companyName: "",
+        start_date: "",
+        end_date: "",
+        job_title: "",
+        company_name: "",
         details: "",
       },
       // education experience data
@@ -189,20 +273,58 @@ export default {
       skills_list: [],
       new_skill: "",
 
+      errors: [],
+      education_list: [],
+      capstones_list: [],
       // each variable above will need data here!
     };
+  },
+  computed: {
+    orderdSkills: function () {
+      var skills_array = this.skills_list;
+      return skills_array.sort((a, b) => (a.name > b.name ? 1 : -1));
+    },
   },
   mounted: function () {
     this.showStudent();
     this.EducationIndex();
+    this.showSkills();
+
+    this.showEducations();
+    this.showCapstones();
+    this.showExperiences();
   },
   methods: {
     // incomplete/ need routes to correctly create these methods
 
     showStudent: function () {
-      axios.get("/api/students/" + this.student.id).then((response) => {
+      axios.get("/api/students/" + this.$route.params.id).then((response) => {
         console.log(response.data);
         this.student = response.data;
+      });
+    },
+    showSkills: function () {
+      axios.get("/api/skills/").then((response) => {
+        console.log(response.data);
+        this.skills_list = response.data;
+      });
+    },
+    showEducations: function () {
+      axios.get("/api/education/").then((response) => {
+        console.log(response.data);
+        this.education_list = response.data;
+      });
+    },
+    showCapstones: function () {
+      axios.get("/api/capstones/").then((response) => {
+        console.log(response.data);
+        this.capstones_list = response.data;
+      });
+    },
+    showExperiences: function () {
+      axios.get("/api/experiences/").then((response) => {
+        console.log(response.data);
+        this.experiences = response.data;
       });
     },
     createCapstone: function () {
@@ -214,9 +336,9 @@ export default {
         screenshot: this.screenshot,
       };
       axios
-        .post("/api/students/" + this.student.id, params)
+        .post("/api/capstone/" + this.student.id, params)
         .then(() => {
-          console.log("capstone added!");
+          // this.$router.push("/profile");
         })
         .catch((error) => console.log(error.response));
     },
@@ -229,47 +351,47 @@ export default {
         screenshot: capstone.screenshot,
       };
       axios
-        .patch("/api/students/" + this.student.id, params)
+        .patch("/api/capstone/" + this.student.id, params)
         .then(() => {
           // this.$router.push("/capstones/" + this.capstone.id);
         })
         .catch((error) => console.log(error.response));
     },
     destroyCapstone: function (capstone) {
-      axios.delete("/api/students/" + capstone.id).then(() => {
+      axios.delete("/api/capstone/" + capstone.id).then(() => {
         console.log("Destroyed");
         // this.$router.push("/students");
       });
     },
     createSkill() {
-      // var params = {
-      //   skill: this.new_skill,
-      // };
-      // axios
-      //   .post("/api/skills", params)
-      //   .then((response) => {
-      //     console.log("skill being added...", response.data);
-      //     this.skills_list.push(response.data);
-      //   })
-      //   .catch((error) => {
-      //     console.log(error.response);
-      //     this.errors = error.response.data.errors;
-      //   });
-      console.log("skill being added...");
-      this.skills_list.unshift(this.new_skill);
+      var params = {
+        skill_name: this.new_skill,
+      };
+      axios
+        .post("/api/skills", params)
+        .then((response) => {
+          console.log("skill being added...", response.data);
+          this.skills_list.unshift(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.errors = error.response.data.errors;
+        });
       this.new_skill = "";
+      // console.log(`"${this.new_skill} being added..."`);
+      // this.skills_list.unshift(this.new_skill);
+      // this.new_skill = "";
     },
     destroySkill(skill) {
-      // axios.delete("/api/skills/" + skill.id).then((response) => {
-      //   console.log("skill being deleted...", response.data);
-      //   var index = this.skills_list.indexOf(skill);
-      //   this.skills_list.splice(index, 1);
-      // });
-      console.log("skill being deleted...");
-      var index = this.skills_list.indexOf(skill);
-      this.skills_list.splice(index, 1);
+      axios.delete("/api/skills/" + skill.id).then((response) => {
+        console.log("skill being deleted...", response.data);
+        var index = this.skills_list.indexOf(skill);
+        this.skills_list.splice(index, 1);
+      });
+      // console.log("skill being deleted...");
+      // var index = this.skills_list.indexOf(skill);
+      // this.skills_list.splice(index, 1);
     },
-
     updateStudent: function (student) {
       let params = {
         first_name: student.first_name,
@@ -284,7 +406,7 @@ export default {
         github_url: student.github_url,
         photo: student.photo,
       };
-      axios.patch("/api/students" + student.id, params).then((response) => {
+      axios.patch("/api/students/" + student.id, params).then((response) => {
         console.log("Success!", response.data);
       });
     },
@@ -292,7 +414,7 @@ export default {
       document.querySelector("#student-details").showModal();
     },
     createEducation: function () {
-      console.log("Creating Eduction info");
+      console.log("Creating Education info");
       var params = {
         start_date: this.start_date,
         end_date: this.end_date,
@@ -327,23 +449,57 @@ export default {
           console.log("all education:", this.educations);
         });
       },
+      axios
+        .patch("/api/education" + education.id, params)
+        .then((response) => {
+          console.log("Success", response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+
+    editExperienceModal: function () {
+      document.querySelector("#experience-details").showModal();
     },
 
     createExperience: function () {
       console.log("Creating an experience");
       var params = {
-        startDate: this.startDate,
-        endDate: this.endDate,
-        jobTitle: this.jobTitle,
-        companyName: this.companyName,
-        details: this.details,
+        start_date: this.experiences.start_date,
+        end_date: this.experiences.end_date,
+        job_title: this.experiences.job_title,
+        company_name: this.experiences.company_name,
+        details: this.experiences.details,
       };
       axios.post("/api/education", params).then(() => {
         this.$router.push("/profile");
+      axios.post("/api/experiences", params).then(() => {
+        // this.$router.push("/profile");
       });
     },
     createWindow: function () {
       document.querySelector("#create-experience-window").showModal();
+    },
+    updateExperience: function (experience) {
+      var params = {
+        start_date: experience.start_date,
+        end_date: experience.end_date,
+        job_title: experience.job_title,
+        company_name: experience.company_name,
+        details: experience.details,
+      };
+      axios.patch("/api/experiences/" + experience.id, params).then((response) => {
+        console.log("Change Accepted", response.data);
+      });
+    },
+    destroyExperience: function (experience) {
+      axios.delete("/api/experiences/" + experience.id).then((response) => {
+        console.log("Delete Confirmed", response.data);
+        var index = this.experiences.indexOf(experience);
+        this.experiences.splice(index, 1);
+      });
+    },
+    createCapstoneWindow: function () {
+      document.querySelector("#create-capstone-window").showModal();
     },
   },
 };
